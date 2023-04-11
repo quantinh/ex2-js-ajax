9cd4875c67da2354bb61c7acbaee12e0e929e53e
COWELL\tinhhq3036 <intern01-tinhhq3036@co-well.vn>
2023-04-11 16:57:24 +0700

    Init project

---
+++ ex2-ajax/data.php
@@ -0,0 +1,6 @@
+$data = array(
+    'id' => 1,
+    'title' => 'nguyen trong nam',
+    'author' => 'seven',
+    'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ullam.'
+)
